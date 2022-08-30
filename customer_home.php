a9af292a0b42a06c296da8cf4f0b1ab3f51799f1
khairul75 <107575587+khairul75@users.noreply.github.com>
2022-08-30 22:03:28 +0600

    Add files via upload

---
+++ customer_home.php
@@ -0,0 +1,22 @@
+<?php include('connect.php');?>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <div class="container-fluid my-3">
+    <h1 class="text-center">NORTH SOUTH ELECTRONICS</h1>
+    <br>
+    <h2 class="text-center"><p><button><a href="products.php"> PRODUCTS</a></button> </p></h>
+    <p><button><a href="logout.php"> LOGOUT</a></button> </p>
+
+          
+</head>
+<body>
+    
+</body>
+</html>
